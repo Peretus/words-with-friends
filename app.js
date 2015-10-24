7b5abf689e91ca7e52e8e0b4a30776f82d83a52b
Casey McNeil <CaseyMcNeil@gmail.com>
2015-10-24 11:30:11 -0500

    Prevent transform of error message

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-$(document).ready(function(){
+$(document).ready(
+  
+  function doYourDance(){
 
-  var timerID,
-  currentRequest;
+    var timerID;
+    var currentRequest;
 
-  function handleError(error){
-    updateWord(error.responseText);
-  }
+    function handleError(error){
+      updateWord(error.responseText);
+    }
 
-  function getNewWord(){
-    updateWord("Please Wait for Word.");
-
-    if(currentRequest){
-      currentRequest.abort();
+    function abortCurrentRequest(){
+      if(currentRequest){
+        currentRequest.abort();
+      };
     };
 
-    repeatedlyAppendPeriods();
-    currentRequest = $.get({url: "http://oseberg.io/interview/word_generator.php", success: updateWord})
-    .fail(handleError);
-  };
+    function currentlyActiveRequest(){
+      return currentRequest.readyState !== 4;
+    }
 
-  function updateWord(newWord){
-    console.log("Updating word....");
-    $("#word").text(newWord);
-    clearInterval(timerID);
-  }
-
-  function appendSinglePeriod(){
-    $("#word").append(".");
-  };
-
-  function repeatedlyAppendPeriods(){
-    timerID = setInterval(appendSinglePeriod,1000)
-  }
-
-  function currentlyDisplayedWord(){
-    return $("#word").text()
-  }
-
-  function transformWord(){
-    console.log("Updating word....");
-    if(currentRequest){
-      currentRequest.abort();
+    function getNewWord(){
+      updateWord("Please Wait for Word.");
+      abortCurrentRequest()
+      repeatedlyAppendPeriods();
+      currentRequest = $.get({url: "http://oseberg.io/interview/word_generator.php", success: updateWord})
+      .fail(handleError);
     };
-    repeatedlyAppendPeriods();
 
-    var builtURL = "http://oseberg.io/interview/shifter.php?word="+currentlyDisplayedWord()
-    currentRequest = $.get({url: builtURL, success: updateWord})
-    .fail(handleError);
-  };
+    function updateWord(newWord){
+      $("#word").text(newWord);
+      clearInterval(timerID);
+    }
 
-  $("#get-word").on("click", getNewWord);
-  $("#transform-word").on("click", transformWord);
-});
+    function appendSinglePeriod(){
+      $("#word").append(".");
+    };
+
+    function repeatedlyAppendPeriods(){
+      timerID = setInterval(appendSinglePeriod,1000)
+    }
+
+    function currentlyDisplayedWord(){
+      return $("#word").text()
+    }
+
+    function transformWord(){
+      if(!currentlyActiveRequest() && currentRequest.status == 200){
+      repeatedlyAppendPeriods();
+      var builtURL = "http://oseberg.io/interview/shifter.php?word="+currentlyDisplayedWord()
+      currentRequest = $.get({url: builtURL, success: updateWord})
+      .fail(handleError);  
+      };
+    };
+
+    $("#get-word").on("click", getNewWord);
+    $("#transform-word").on("click", transformWord);
+  }
+  
+);
